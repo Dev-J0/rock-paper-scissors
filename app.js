e7504af6f80d3f7b551a40e82407411bbbe54ac7
d0bsidian <101651861+d0bsidian@users.noreply.github.com>
2022-06-30 12:23:12 +0200

    é"

---
+++ app.js
@@ -0,0 +1,22 @@
+function computerPlay () {
+    let rock = "rock";
+    let paper = "paper";
+    let scissors = "scissors";
+}
+
+function playRound(playerSelection, computerSelection) {
+    return "You lose! Paper beats rock";
+}
+
+const playerSelection = "rock";
+const computerSelection = computerPlay();
+console.log(playRound(playerSelection, computerSelection));
+
+function game() {
+    function playRound() {
+        for (let i = 0; i < 5; i++) {
+           let question = prompt ("testing");
+        }
+
+    }
+}
